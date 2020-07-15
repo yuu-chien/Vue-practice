0ada622d864be98d6fe5339a7e20797ecfeab9c0
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2020-07-15 14:20:47 +0800

    component

---
+++ practice-hex-JSLC-w4/JS/forList2.js
@@ -0,0 +1,10 @@
+export default {
+    template: "#forList2",
+    data: function() {
+        return {
+            list1: '晚',
+            list2: '安',
+            list3: '喔',
+        }
+    }
+}
